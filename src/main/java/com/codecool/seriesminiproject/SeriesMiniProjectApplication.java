41507153a060e9a6b508cdc445500bd266bf537c
mkmDEV <mkmdev18@gmail.com>
2019-06-12 17:27:53 +0200

    initial commit

---
+++ src/main/java/com/codecool/seriesminiproject/SeriesMiniProjectApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.seriesminiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeriesMiniProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeriesMiniProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/seriesminiproject/SeriesMiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.seriesminiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeriesMiniProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
