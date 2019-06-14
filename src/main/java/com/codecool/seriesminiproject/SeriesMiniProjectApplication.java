a386d03a6b93e0093e177f749333e30251482a60
mkmDEV <mkmdev18@gmail.com>
2019-06-14 15:31:21 +0200

    refactored object order,  and calls

--- src/main/java/com/codecool/seriesminiproject/SeriesMiniProjectApplication.java
+++ src/main/java/com/codecool/seriesminiproject/SeriesMiniProjectApplication.java
@@ -5,6 +5,7 @@ import com.codecool.seriesminiproject.entities.Episode;
 import com.codecool.seriesminiproject.entities.Season;
 import com.codecool.seriesminiproject.entities.Series;
 import com.codecool.seriesminiproject.repositories.SeriesRepository;
+import org.assertj.core.util.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -26,38 +27,37 @@ public class SeriesMiniProjectApplication {
     }
 
     @Bean
-    @Profile("produciton")
+    @Profile("production")
     public CommandLineRunner init() {
         return args -> {
-            Season chernobyl1 = Season.builder()
-                    .title("Chernobyl")
-                    .releaseDate(LocalDate.of(2019, 5, 16))
-                    .build();
-
-            Season starWars1 = Season.builder()
-                    .title("Star Wars")
-                    .releaseDate(LocalDate.of(1979,8,16))
-                    .build();
-
             Episode newHope = Episode.builder()
                     .title("A New Hope")
                     .releaseDate(LocalDate.of(1979, 8, 16))
-                    .season(starWars1)
-                    .starring(Arrays.asList("Mark Hamill", "Harrison Ford", "Carrie Fisher"))
+                    .stars(Arrays.asList("Mark Hamill", "Harrison Ford", "Carrie Fisher"))
                     .build();
 
             Episode empireStrikesBack = Episode.builder()
                     .title("The Empire Strikes Back")
                     .releaseDate(LocalDate.of(1982,1,28))
-                    .season(starWars1)
-                    .starring(Arrays.asList("Mark Hamill", "Harrison Ford", "Carrie Fisher", "Billy Dee Williams"))
+                    .stars(Arrays.asList("Mark Hamill", "Harrison Ford", "Carrie Fisher", "Billy Dee Williams"))
                     .build();
 
             Episode chernobylE1 = Episode.builder()
                     .title("01")
                     .releaseDate(LocalDate.of(2019,5,16))
-                    .season(chernobyl1)
-                    .starring(Arrays.asList("Jared Harris", "Jessie Buckley", "Stellan Skarsgård"))
+                    .stars(Arrays.asList("Jared Harris", "Jessie Buckley", "Stellan Skarsgård"))
+                    .build();
+
+            Season chernobyl1 = Season.builder()
+                    .title("Chernobyl")
+                    .releaseDate(LocalDate.of(2019, 5, 16))
+                    .episode(chernobylE1)
+                    .build();
+
+            Season starWars1 = Season.builder()
+                    .title("Star Wars")
+                    .releaseDate(LocalDate.of(1979,8,16))
+                    .episodes(Lists.newArrayList(newHope, empireStrikesBack))
                     .build();
 
             Series chernobyl = Series.builder()
@@ -71,6 +71,10 @@ public class SeriesMiniProjectApplication {
                     .channel(Channel.CenturyFox)
                     .season(starWars1)
                     .build();
+
+            newHope.setSeason(starWars1);
+            empireStrikesBack.setSeason(starWars1);
+            chernobylE1.setSeason(chernobyl1);
 
             chernobyl1.setSeries(chernobyl);
             starWars1.setSeries(starWars);
