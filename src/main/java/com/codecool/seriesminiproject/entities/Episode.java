e72bd3cbb07d0609945040dc6704cf74c7a9f9cd
mkmDEV <mkmdev18@gmail.com>
2019-06-14 14:19:41 +0200

    + sketch up basic episode entity

---
+++ src/main/java/com/codecool/seriesminiproject/entities/Episode.java
@@ -0,0 +1,30 @@
+package com.codecool.seriesminiproject.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Episode {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String title;
+
+    @ManyToOne
+    private Season season;
+
+    private LocalDate releaseDate;
+
+    @ElementCollection
+    @Singular
+    private List<String> starring;
+}
